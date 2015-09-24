167716e7e9495e3e49438a87ca0c7455f75cc4bb
Matt Brock <mbrock8@gmail.com>
2015-09-24 08:39:41 -0500

    Removing unnecessary code from site.js that doesn't apply to this project

--- _js/site.js
+++ _js/site.js
@@ -15,31 +15,21 @@ function initialize(){
 function slideMenu(){
 	
 	// Must be separate from content that is pushed
-	var $slideMenu, $pageWrapper, $floatingTrigger, $floatingLogo;
+	var $slideMenu, $pageWrapper;
 	
 	$slideMenu = $('#slideMenu').detach();	
 	$pageWrapper = '<div id="page-wrapper"></div>';
 	
-	$floatingTrigger  = '<div class="slideMenu--floating-trigger">';
-	$floatingTrigger += '<span class="slideMenu--text">Menu</span>';
-	$floatingTrigger += '<img src="/_img/menu-hamb-515050.png" class="slideMenu--hamb-icon">';
-	$floatingTrigger += '</div>';
-	
-	$floatingLogo  = '<div class="floating-logo"><a href="#">';
-	$floatingLogo += '<img src="/_img/fbc-cross_color.png">';
-	$floatingLogo += '</a></div>';
-	
-	$('body').wrapInner($pageWrapper).append($slideMenu).prepend($floatingTrigger);
-	$('body').prepend($floatingLogo);
+	$('body').wrapInner($pageWrapper).append($slideMenu);
 	
 	function slideMenuGo(){
 		$('#slideMenu').toggleClass('change');
-		$('#page-wrapper, .slideMenu--floating-trigger').toggleClass('pushed');
+		$('#page-wrapper').toggleClass('pushed');
 		$('#slideMenu--mask').toggleClass('is-active');
 		$('body').toggleClass('has-active-slideMenu');
 	}
 	
-	$('.slideMenu--trigger, .slideMenu--floating-trigger, #slideMenu--mask, .slideMenu--close').on('click', function(e){
+	$('.slideMenu--trigger, #slideMenu--mask, .slideMenu--close').on('click', function(e){
 		e.preventDefault();
 		slideMenuGo();
 	});
