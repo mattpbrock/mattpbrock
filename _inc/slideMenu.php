f2bdd5fac349a72ed481e2042abc366a0557dcd2
Matt Brock <mbrock8@gmail.com>
2015-09-24 08:18:05 -0500

    Added slideMenu.php to mattpbrock site

---
+++ _inc/slideMenu.php
@@ -0,0 +1,46 @@
+<!-- Begin #slideMenu -->
+
+<div id="slideMenu">
+	
+	<div id="slideMenu--header">
+		<a class="slideMenu--login button-border" href="#">Login</a>
+		<span class="slideMenu--close">x</span>
+	</div>
+	
+<!-- 	<p class="slideMenu--title">Welcome!</p> -->
+	
+	<ul class="slideMenu--nav main">
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.google.com" title="Menu Item">The Gospel</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.yahoo.com" title="Menu Item">About</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.fellowshipnashville.org" title="Menu Item">Connect</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.apple.com" title="Menu Item">Messages</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.portico.com" title="Menu Item">Resources</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.portico.com" title="Menu Item">Giving</a></li>
+	</ul>
+	
+	<ul class="slideMenu--nav secondary">
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.google.com" title="Menu Item">Brentwood</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.yahoo.com" title="Menu Item">Franklin</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.fellowshipnashville.org" title="Menu Item">South Nashville</a></li>
+	</ul>
+	
+<!--
+	<ul class="slideMenu--nav extra">
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.portico.com" title="Menu Item">Give Online</a></li>
+		<li class="slideMenu--item"><a class="slideMenu--link" href="http://www.portico.com" title="Menu Item">Login</a></li>
+	</ul>
+-->
+	
+	<div class="slideMenu--signup">
+		<p>Enter your email address to receive the First &amp; Third, our bi-weekly church newsletter!</p>
+		<form id="slideMenu--first-and-third">
+			<fieldset>
+				<input type="email" placeholder="Email Address">
+				<input type="submit" value="Sign me up">
+			</fieldset>
+		</form>
+	</div>
+	
+</div>
+
+<!-- End #slideMenu -->
