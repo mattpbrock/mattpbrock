43789e4e7e2a45b51d57a7902491655e7a39a5cc
Matt Brock <mbrock8@gmail.com>
2015-11-03 11:11:36 -0600

    Cleaning up to deploy to production

--- _js/site.js
+++ _js/site.js
@@ -12,7 +12,6 @@ function initialize(){
 }
 
 
-
 function slideHeader(){
 	
 	$('#nav-icon').on('click', function(e){
@@ -28,47 +27,3 @@ function slideHeader(){
 	});
 	
 }
-
-
-
-
-
-
-
-
-
-
-// function slideHeader(){
-	
-	// PROBLEM: Need a simple action -- when button in top right of header
-	// is clicked, Site-header slides left with body.
-	
-	// Must be separate from content that is pushed
-/*
-	var $slideHeader, $pageWrapper;
-	
-	$slideHeader = $('#Site-header');
-	$pageWrapper = '<div id="page-wrapper"></div>';
-	
-	$('body').wrapInner($pageWrapper).append($slideHeader);
-	
-	function slideHeaderGo(){
-		$('#slideHeader').toggleClass('change');
-		$('#page-wrapper').toggleClass('pushed');
-		$('#slideHeader--mask').toggleClass('is-active');
-		$('body').toggleClass('has-active-slideHeader');
-	}
-	
-	$('.slideHeader--trigger, #slideHeader--mask, .slideHeader--close').on('click', function(e){
-		e.preventDefault();
-		slideHeaderGo();
-	});
-	
-	$('.slideHeader--link').click(function(e) {
-		e.preventDefault();
-		slideHeaderGo();
-		var goTo = this.getAttribute("href");
-		setTimeout(function(){ window.location = goTo; }, 300);
-	});
-}
-*/
